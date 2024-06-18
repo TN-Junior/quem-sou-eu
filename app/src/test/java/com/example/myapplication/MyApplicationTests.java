7b5578ae5c2b668da494b44f63f6ec636d04f051
Tary Junior <111458237+tn-junior@users.noreply.github.com>
2024-06-18 12:26:04 -0300

    Update MyApplicationTests.java

--- app/src/test/java/com/example/myapplication/MyApplicationTests.java
+++ app/src/test/java/com/example/myapplication/MyApplicationTests.java
@@ -19,7 +19,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-@RunWith(AndroidJUnit4.class)
+@RunWith()
 public class MyApplicationTests {
 
     private ActivityScenario<MainActivity> mainActivityScenario;
